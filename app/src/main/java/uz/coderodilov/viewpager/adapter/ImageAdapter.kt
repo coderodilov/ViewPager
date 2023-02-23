4e4add2e0dd1f079be37c9cecca72292330777e0
coderodilov <coder.odilov@gmail.com>
2023-02-23 16:10:35 +0500

    Finish

--- app/src/main/java/uz/coderodilov/viewpager/adapter/ImageAdapter.kt
+++ app/src/main/java/uz/coderodilov/viewpager/adapter/ImageAdapter.kt
@@ -7,14 +7,14 @@ import android.widget.ImageView
 import androidx.viewpager.widget.PagerAdapter
 import uz.coderodilov.viewpager.data.DataProvider
 
-class ImageAdapter (private val context:Context) : PagerAdapter(){
-    val dataProvider = DataProvider()
-    val images = dataProvider.getImagesList()
+class ImageAdapter(private val context: Context) : PagerAdapter() {
+    private val dataProvider = DataProvider()
+    private val images = dataProvider.getImagesList()
 
     override fun getCount(): Int = images.size
 
     override fun isViewFromObject(view: View, image: Any): Boolean {
-       return view == image
+        return view == image
     }
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
@@ -26,7 +26,7 @@ class ImageAdapter (private val context:Context) : PagerAdapter(){
     }
 
     override fun destroyItem(container: ViewGroup, position: Int, image: Any) {
-       container.removeView(image as View)
+        container.removeView(image as View)
     }
 
 }
